fbaba0f22e41c4ceb36c443d87e76a6c892dfde1
leonardo-dsouza <dkaireturn@gmail.com>
2026-07-02 01:41:07 -0300

    chore: add preview class for events

---
+++ src/core/eventos/alerta_estoque.py
@@ -0,0 +1,13 @@
+import uuid
+from dataclasses import dataclass, field
+from datetime import datetime
+from decimal import Decimal
+
+
+@dataclass
+class AlertaEstoque:
+    ingrediente_id: uuid.UUID
+    nome_ingrediente: str
+    quantidade_atual: Decimal
+    estoque_minimo: Decimal
+    data_alerta: datetime = field(default_factory=datetime.utcnow)
